0e182c6b74efbe9b02d4df9375b53b3de533ba3c
AnilChinchawale <anil24593@gmail.com>
2019-10-16 18:27:52 +0530

    IPFS Test

--- apis/ipfs.js
+++ apis/ipfs.js
@@ -3,45 +3,62 @@ const express = require('express')
 const router = express.Router()
 const path = require('path')
 const fs = require('fs')
-const exec = require('child_process').exec
+const IpfsClient = require('ipfs-http-client')
+
+const xinFinClient = new IpfsClient({
+    host: 'ipfs.xinfin.network',
+    port: 443,
+    protocol: 'https'
+})
 
 if (!fs.existsSync(path.join(__dirname, '../tmp/'))) {
     fs.mkdirSync(path.join(__dirname, '../tmp/'))
 }
 
-router.post('/addKYC', async function (req, res, next) {
+router.get('/addKYC', async function (req, res, next) {
     console.log(req.files)
     console.log('File Name : ', req.files.filename)
     let imageFile = req.files.filename
-    var hash
-    const name = 'dummy.png'
 
-    imageFile.mv(path.join(__dirname, '../tmp/', name), function (err) {
-        if (err) {
-            return res.status(500).send(err)
+    xinFinClient.add(imageFile, async (err, ipfsHash) => {
+         if (err != null) {
+            // error occured, log out the error
+            console.error('Some error occured while adding KYC at /addKYC: ', err)
+            res.status(500).send(err)
+            return // need to handle this error
         }
-        const filePath = path.join(__dirname, '/../tmp/', name)
-        exec(
-            `IPFS_PATH=~/.ipfs1 ipfs add ${filePath}`,
-            async (error, stdout, stderr) => {
-                if (error != null) {
-                    res.status(500).send(error)
-                }
-                var words = stdout.split(' ')
-                console.log('WORDS', words)
-                for (var i = 0; i < words.length; i++) {
-                    if (words[i][0] === 'Q') hash = words[i]
-                }
-                console.log('HASH : ', hash)
-                console.log('deleting : ', filePath)
-                fs.unlink(filePath, err => {
-                    if (err) throw err
-                    console.log('File successfully deleted')
-                    res.status(200).json({ 'hash':hash })
-                })
-            }
-        )
+        let hash = ipfsHash[0].hash
+        // all ok
+        console.log(`Uploaded file; hash: ${hash}`)
+        res.status(200).json({ 'hash': hash })
     })
+
+    // imageFile.mv(path.join(__dirname, '../tmp/', name), function (err) {
+    //     if (err) {
+    //         return res.status(500).send(err)
+    //     }
+    //     const filePath = path.join(__dirname, '/../tmp/', name)
+    //     exec(
+    //         `IPFS_PATH=~/.ipfs1 ipfs add ${filePath}`,
+    //         async (error, stdout, stderr) => {
+    //             if (error != null) {
+    //                 res.status(500).send(error)
+    //             }
+    //             var words = stdout.split(' ')
+    //             console.log('WORDS', words)
+    //             for (var i = 0; i < words.length; i++) {
+    //                 if (words[i][0] === 'Q') hash = words[i]
+    //             }
+    //             console.log('HASH : ', hash)
+    //             console.log('deleting : ', filePath)
+    //             fs.unlink(filePath, err => {
+    //                 if (err) throw err
+    //                 console.log('File successfully deleted')
+    //                 res.status(200).json({ 'hash':hash })
+    //             })
+    //         }
+    //     )
+    // })
 })
 
 module.exports = router
