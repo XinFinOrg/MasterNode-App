f8dc9e80bffccebf191d734f50b36c6946f896f3
AnilChinchawale <anil24593@gmail.com>
2020-02-11 14:31:44 +0530

    XinFin Masternode Governance DApp v2.0

--- models/blockchain/web3ws.js
+++ models/blockchain/web3ws.js
@@ -3,10 +3,26 @@
 const Web3 = require('xdc3')
 const config = require('config')
 
-function Web3Ws () {
-    let provider = new Web3.providers.WebsocketProvider(config.get('blockchain.ws'))
-    let web3 = new Web3(provider)
-    return web3
+const web3Ws = {
+    Web3Ws: function () {
+        // console.log(`Here me at WEB3WS`)
+        let provider = new Web3.providers.WebsocketProvider(config.get('blockchain.ws'))
+        let web3 = new Web3(provider)
+        return web3
+    },
+    Web3WsInternal: function () {
+        // console.log(`Here me at Web3WsInternal`)
+        let provider = new Web3.providers.WebsocketProvider(config.get('blockchain.internalWs'))
+        let web3 = new Web3(provider)
+        // console.log(web3)
+        return web3
+    }
 }
 
-module.exports = Web3Ws
+// function Web3Ws () {
+//     let provider = new Web3.providers.WebsocketProvider(config.get('blockchain.ws'))
+//     let web3 = new Web3(provider)
+//     return web3
+// }
+
+module.exports = web3Ws
