d7c63921ce256518db740137c04cb0f4ad3355e8
Satish pethani <satyapethani@gmail.com>
2024-10-04 14:02:32 +0530

    Issue fixed.

--- models/mongodb/rank.js
+++ models/mongodb/rank.js
@@ -4,7 +4,7 @@ var mongoose = require('mongoose')
 var Schema = mongoose.Schema
 
 var Rank = new Schema({
-    candidate: { type: Number, index: true },
+    candidate: { type: String, index: true },
     rank: Number,
     epoch: { type: Number, index: true },
     epochCreatedAt: Date
